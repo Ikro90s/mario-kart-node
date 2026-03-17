c073b5905b2b28bd00c8e7528bc171dfdc4207b1
Icaro Pereira De Paula <icaropereiradepaula@gmail.com>
2026-03-17 13:48:02 -0400

    Adicionando os dados de cada personagem do jogo ( players)

---
+++ src/data/players.js
@@ -0,0 +1,44 @@
+export default players = {
+  playerFirst: {
+    NAME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+  },
+  playerSecond: {
+    NAME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+  },
+  playerThird: {
+    NAME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+  },
+  playerFourth: {
+    NAME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+  },
+  playerFifth: {
+    NAME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  },
+  playerSixth: {
+    NAME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+  },
+};

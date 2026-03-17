ea1d7718d7a806b556039637ef7710bdbd18324a
Icaro Pereira De Paula <icaropereiradepaula@gmail.com>
2026-03-17 19:58:43 -0400

    Feito refatorações, adição de resultado dos dados na engine, e variável para armazenar a habilidade do jogador

--- src/game/raceEngine.js
+++ src/game/raceEngine.js
@@ -1,10 +1,20 @@
 import getRandomBlock from "../utils/block.js";
-
+import rollDice from "../utils/dice.js";
 async function playerRaceEngine(character1, character2) {
+  //Inicializa cada rodada
   for (let round = 1; round <= 5; round++) {
     console.log(`Rodada: ${round}`);
+
+    //Gera um bloco de corrida aleatório ( o da vez é o mesmo para os dois jogadores)
     let block = await getRandomBlock();
     console.log(`Bloco: ${block}`);
+
+    //Rola os dados
+    let diceResultFirst = await rollDice();
+    let diceResultSecond = await rollDice();
+
+    let totalTestSkillFirst = 0;
+    let totalTestSkillSecond = 0;
   }
 }
 
